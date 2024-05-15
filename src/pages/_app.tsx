ab6f7859c10f53d2dc4a0b044a4556c6af140d8e
JOAO TARRAN <joaotarran@gmail.com>
2024-05-15 17:40:11 -0300

    hello world

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
+import "material-icons/iconfont/material-icons.css";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
