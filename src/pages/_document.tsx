6126ae23b0adc60369c5b42b41f74623a5809a87
JOAO TARRAN <joaotarran@gmail.com>
2024-10-17 18:31:15 -0300

    mailchimp script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
     return (
@@ -6,7 +7,13 @@ export default function Document() {
             <Head>
                 <link rel="manifest" href="/manifest.json" />
                 <meta name="theme-color" content="#bea07d" />
+                <Script
+                    id="mcjs"
+                    strategy="afterInteractive"
+                    src="https://chimpstatic.com/mcjs-connected/js/users/f93e5aefaaf238b53c65406d4/e4b0879b6d7d67b8101a9d717.js"
+                />
             </Head>
+
             <body>
                 <Main />
                 <NextScript />
