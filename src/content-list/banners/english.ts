0153b7691cf4487910c2e6aba8f6794033fd728a
JOAO TARRAN <joaotarran@gmail.com>
2024-10-17 17:34:16 -0300

    pequenas correcoes e adicao da area de entretenimento

--- src/content-list/banners/english.ts
+++ src/content-list/banners/english.ts
@@ -13,13 +13,6 @@ export const Banners_Data_EN = [
         link: "/direito-internacional",
     },
     {
-        title: "Civil Law",
-        description:
-            "We operate in various areas of civil law, including Banking Law, Consumer Law, Contract Law, Real Estate Law, Family and Succession Law, Compensation, Arbitration, and Mediation.",
-        image: "/general_assets/banner_direito_civil.png",
-        link: "/direito-civil",
-    },
-    {
         title: "Customs Law",
         description:
             "We offer assistance in import and export, with specialized tax planning for Foreign Trade transactions. This includes the granting of special customs regimes, identification of tax benefits, and legal compliance.",
@@ -27,17 +20,31 @@ export const Banners_Data_EN = [
         link: "/direito-aduaneiro",
     },
     {
-        title: "Business Law",
-        description:
-            "We operate in various areas of Business Law, including Corporate Law, Contract Law, Tax Law, Labor Law, Competition Law, Consumer Law, Environmental Law, and International Law.",
-        image: "/general_assets/banner_direito_empresarial.png",
-        link: "/direito-empresarial",
-    },
-    {
         title: "Sports Law",
         description:
             "Drafting and negotiating contracts, sponsorships, image and arena rights, brand protection, and advising on labor issues specific to sports, as well as matters such as anti-doping control, licensing, and alternative methods for resolving legal-sports disputes.",
         image: "/general_assets/banner_direito_desportivo.png",
         link: "/direito-desportivo",
+    },
+    {
+        title: "Entertainment Law",
+        description:
+            "Drafting and negotiating contracts, sponsorships, image and author's rights, brand protection, and advising on labor issues specific to entertainment.",
+        image: "/general_assets/banner_direito_entretenimento.jpg",
+        link: "/direito-entretenimento",
+    },
+    {
+        title: "Civil Law",
+        description:
+            "We operate in various areas of civil law, including Banking Law, Consumer Law, Contract Law, Real Estate Law, Family and Succession Law, Compensation, Arbitration, and Mediation.",
+        image: "/general_assets/banner_direito_civil.png",
+        link: "/direito-civil",
+    },
+    {
+        title: "Business Law",
+        description:
+            "We operate in various areas of Business Law, including Corporate Law, Contract Law, Tax Law, Labor Law, Competition Law, Consumer Law, Environmental Law, and International Law.",
+        image: "/general_assets/banner_direito_empresarial.png",
+        link: "/direito-empresarial",
     },
 ];
--- src/content-list/banners/portuguese.ts
+++ src/content-list/banners/portuguese.ts
@@ -14,13 +14,6 @@ export const Banners_Data_PT = [
         link: "/direito-internacional",
     },
     {
-        title: "Direito Civil",
-        description:
-            "Atuamos em diversas áreas do direito civil, abrangendo Direito Bancário, Direito do Consumidor, Direito Contratual, Direito Imobiliário, Direito de Família e Sucessões, Indenizações, Arbitragens e Mediações.",
-        image: "/general_assets/banner_direito_civil.png",
-        link: "/direito-civil",
-    },
-    {
         title: "Direito Aduaneiro",
         description:
             "Oferecemos assistência em importação e exportação, com planejamento tributário especializado para transações de Comércio Exterior. Incluímos concessão de regimes aduaneiros especiais, identificação de benefícios fiscais e conformidade legal no Direito Aduaneiro.",
@@ -28,17 +21,31 @@ export const Banners_Data_PT = [
         link: "/direito-aduaneiro",
     },
     {
-        title: "Direito Empresarial",
-        description:
-            "Atuamos em diversas áreas do Direito Empresarial, incluindo Direito Societário, Direito Contratual, Direito Tributário, Direito do Trabalho, Direito da Concorrência, Direito do Consumidor, Direito Ambiental e Direito Internacional.",
-        image: "/general_assets/banner_direito_empresarial.png",
-        link: "/direito-empresarial",
-    },
-    {
         title: "Direito Desportivo",
         description:
             "Elaboração e negociação de contratos, patrocínios, direitos de imagem e de arena, proteção de marcas e assessoria em questões trabalhistas específicas do esporte, além de questões como controle antidoping, licenciamento e métodos alternativos para a resolução de conflitos jurídico-desportivos.",
         image: "/general_assets/banner_direito_desportivo.png",
         link: "/direito-desportivo",
+    },
+    {
+        title: "Direito do Entretenimento",
+        description:
+            "Elaboração e negociação de contratos, patrocínios, direitos de imagem e autorais, proteção de marcas e assessoria em questões trabalhistas específicas do entretenimento.",
+        image: "/general_assets/banner_direito_entretenimento.jpg",
+        link: "/direito-entretenimento",
+    },
+    {
+        title: "Direito Civil",
+        description:
+            "Atuamos em diversas áreas do direito civil, abrangendo Direito Bancário, Direito do Consumidor, Direito Contratual, Direito Imobiliário, Direito de Família e Sucessões, Indenizações, Arbitragens e Mediações.",
+        image: "/general_assets/banner_direito_civil.png",
+        link: "/direito-civil",
+    },
+    {
+        title: "Direito Empresarial",
+        description:
+            "Atuamos em diversas áreas do Direito Empresarial, incluindo Direito Societário, Direito Contratual, Direito Tributário, Direito do Trabalho, Direito da Concorrência, Direito do Consumidor, Direito Ambiental e Direito Internacional.",
+        image: "/general_assets/banner_direito_empresarial.png",
+        link: "/direito-empresarial",
     },
 ];
