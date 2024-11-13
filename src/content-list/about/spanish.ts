6f53e180314abfa85d8d8bf40ba64ac7a93ad065
JOAO TARRAN <joaotarran@gmail.com>
2024-11-13 08:21:11 -0300

    about small update

--- src/content-list/about/spanish.ts
+++ src/content-list/about/spanish.ts
@@ -5,11 +5,11 @@ const About_Data_ES = {
 
     title: "Nuestra Historia",
     paragraphs: [
-        "Fundado en Curitiba en 1996 por la Dra. Liana Maria Taborda Lima, el despacho Taborda Lima & Abogados Asociados se destaca por su excelencia y profesionalismo. Con un equipo experimentado y altamente calificado, el despacho tiene casi tres décadas de experiencia en el mercado jurídico, siempre actualizado con los cambios e innovaciones legales más recientes.",
-        "Inicialmente enfocado en el Derecho Aduanero, el despacho rápidamente se estableció como referencia en el área, ofreciendo servicios jurídicos de alta calidad. El conocimiento especializado y el estudio continuo de los temas aduaneros permiten a Taborda Lima & Abogados Asociados brindar un servicio distintivo y eficiente.",
-        "Los profesionales del despacho son especialistas, maestros y doctores en diversas áreas del Derecho, incluyendo Derecho Aduanero, Migratorio, Administrativo y Tributario. Con una visión amplia e internacional, se dedican a ofrecer excelencia en todas las áreas de actuación.",
-        "La actuación en Derecho Internacional e Inmigración refuerza el compromiso del despacho de atender bien a sus clientes. Con socios en Nueva York, Newark, Miami, Orlando, Washington, Londres, París, Roma y varias otras ciudades, incluyendo América Latina, Taborda Lima & Abogados Asociados ofrece una red de apoyo global para cuestiones jurídicas complejas.",
-        "Con un firme compromiso con la ética y la responsabilidad, Taborda Lima & Abogados Asociados se dedica a la resolución de conflictos y a la satisfacción de los clientes. Su misión es trabajar con calidad, entusiasmo y lealtad, garantizando un servicio jurídico de excelencia.",
+        "Fundado en Curitiba en 1996, Taborda Lima & Advogados Associados se destaca por su excelencia y profesionalismo. Con un equipo experimentado y altamente calificado, el despacho cuenta con casi tres décadas de experiencia en el mercado jurídico, siempre actualizado con los últimos cambios e innovaciones legales.",
+        "Inicialmente enfocado en el Derecho Aduanero, el despacho se estableció rápidamente como una referencia en el área, ofreciendo servicios jurídicos de alta calidad. El conocimiento especializado y el estudio continuo de temas aduaneros permiten a Taborda Lima & Advogados Associados brindar un servicio distinto y eficiente.",
+        "Los profesionales del despacho son especialistas, con maestrías y doctorados en diversas áreas del Derecho, incluyendo Derecho Aduanero, Migratorio e Internacional. Con una visión amplia e internacional, se dedican a ofrecer excelencia en todas las áreas de práctica.",
+        "La práctica en Derecho Internacional e Inmigración refuerza el compromiso del despacho en atender bien a sus clientes. Con socios en Nueva York, Newark, Miami, Orlando, Washington, Londres, París, Roma y varias otras ciudades, incluyendo América Latina, Taborda Lima & Advogados Associados ofrece una red de apoyo global para asuntos jurídicos complejos.",
+        "Con un firme compromiso con la ética y la responsabilidad, Taborda Lima & Advogados Associados se dedica a la resolución de conflictos y a la satisfacción de sus clientes. Su misión es trabajar con calidad, entusiasmo y lealtad, garantizando un servicio jurídico de excelencia.",
     ],
     team_title: "Nuestro Equipo",
 };
